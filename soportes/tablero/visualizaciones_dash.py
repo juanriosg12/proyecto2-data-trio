8721ce8346fe70978049ff91e3e2fe16814f3920
juanriosg12 <j.rg9712@gmail.com>
2024-11-03 21:25:28 -0500

    add tablero

--- soportes/tablero/visualizaciones_dash.py
+++ soportes/tablero/visualizaciones_dash.py
@@ -6,6 +6,8 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
+
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
